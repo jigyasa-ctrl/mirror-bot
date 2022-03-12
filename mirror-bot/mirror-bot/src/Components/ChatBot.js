37d8f64e454699956f511acf6dd38da8200e0fc3
Jigyasa Upadhyay <jigyasa.upadhyay@active.ai>
2022-03-12 15:09:05 +0530

    Added typing and header bot image

--- mirror-bot/mirror-bot/src/Components/ChatBot.js
+++ mirror-bot/mirror-bot/src/Components/ChatBot.js
@@ -1,16 +1,27 @@
 import './App.css';
 import Chat from './Chat'
 import React, { useEffect, useState } from 'react';
+import bot from "../../src/bot.png"
 
 function ChatBot() {
     const localData = localStorage.getItem("history") ? JSON.parse(localStorage.getItem("history")) : []
     const [Input, setInput] = useState("")
     const [history, setHistory] = useState(localData)
+    const [typing, setTyping] = useState("hidden")
+    const [show, setShow] = useState(true)
     useEffect(() => {
-        console.log(history)
         if (history.length > 0) {
             localStorage.setItem("history", JSON.stringify(history))
             setInput("")
+        }
+        var divs = document.getElementsByClassName("bot")
+        var lastChild = divs[divs.length - 1];
+        if (lastChild) {
+
+            if (!show) {
+                lastChild.style.visibility = "hidden"
+                setTyping("visible")
+            }
         }
     }, [history])
 
@@ -18,17 +29,31 @@ function ChatBot() {
         if (Input.length > 0) {
             setHistory(history => history.concat(Input))
         }
+        setShow(false)
+        setTimeout(() => {
+            var divs = document.getElementsByClassName("bot")
+            var lastChild = divs[divs.length - 1];
+            if (lastChild) {
+                lastChild.style.visibility = "visible"
+                setTyping("hidden")
+            }
+
+        }, 2000);
 
     }
     return (
         <div className="Body">
-            <header>Mirror Bot</header>
+            <span id="span-header">
+                <img src={bot} ></img>
+                <p>Mirror Bot</p>
+            </span>
             <Chat history={history} />
+            <span id="typing" style={{ visibility: `${typing}` }}>Amber is Typing ...</span>
             <div className="footer">
                 <input type="text" name="chat" placeholder="Enter your message ..." value={Input} onChange={(e) => setInput(e.target.value)} onKeyDown={(e) => e.key === 'Enter' ? onSend() : ""} ></input>
                 <button onClick={onSend}>Send</button>
             </div>
-        </div>
+        </div >
     );
 }
 
